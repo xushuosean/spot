add1e6af8d59e3fb9e204221cc1f9aa7c3a2ebf2
Sean <1963966513@qq.com>
2022-09-18 20:53:32 +0800

    调整协同的组件

--- src/pages/Components/ContentCollection/PreviewCollaborate/index.tsx
+++ src/pages/Components/ContentCollection/PreviewCollaborate/index.tsx
@@ -1,6 +1,7 @@
 import { ListItem } from "@/pages/BaseTypes"
 import { useIcon } from "@/pages/hooks"
 import { Avatar } from "antd"
+import { UserOutlined } from '@ant-design/icons'
 import { FC } from "react"
 import styles from './index.less'
 
@@ -18,10 +19,10 @@ export const PreviewCollaborate: FC<PreviewCollaborateProps> = ({
       <img src={icon} />
     </div>
     <div className={styles.avatarBox}>
-      <Avatar size='large' style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>A</Avatar>
-      <Avatar size='large' style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>B</Avatar>
-      <Avatar size='large' style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>C</Avatar>
-      <Avatar size='large' style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>D</Avatar>
+      <Avatar size='large' style={{ color: '#f56a00', backgroundColor: '#fde3cf' }} icon={<UserOutlined />}>A</Avatar>
+      <Avatar size='large' style={{ color: '#f56a00', backgroundColor: '#fde3cf' }} icon={<UserOutlined />}>B</Avatar>
+      <Avatar size='large' style={{ color: '#f56a00', backgroundColor: '#fde3cf' }} icon={<UserOutlined />}>C</Avatar>
+      <Avatar size='large' icon={<UserOutlined />}>D</Avatar>
     </div>
   </div>
 }
